17f76964c1866f44a63bfd3295f6401a00015774
Atharvbuilds <atharvbuilds@gmail.com>
2026-04-12 20:44:58 +0530

    add tab switching functionality and update haeding

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ let currentTab = "need";
 
 let needPosts = [];
 let offerPosts = [];
+
+let needTabBtn = document.getElementById("needTab");
+let offerTabBtn = document.getElementById("offerTab");
+let feedHeading = document.getElementById("feedHeading");
+
+needTabBtn.addEventListener("click", function () {
+  currentTab = "need";
+  switchTab();
+});
+
+offerTabBtn.addEventListener("click", function () {
+  currentTab = "offer";
+  switchTab();
+});
+
+function switchTab() {
+  needTabBtn.classList.toggle("active-tab", currentTab === "need");
+  offerTabBtn.classList.toggle("active-tab", currentTab === "offer");
+
+  feedHeading.innerText =
+    currentTab === "need" ? "I NEED POSTS" : "I OFFER POSTS";
+}
