5da1818684788ce4b6fc2df616ef4090160cc725
Atharvbuilds <atharvbuilds@gmail.com>
2026-04-13 00:49:08 +0530

    handle post submission and store posts based on tab

--- app.js
+++ app.js
@@ -24,3 +24,29 @@ function switchTab() {
   feedHeading.innerText =
     currentTab === "need" ? "I NEED POSTS" : "I OFFER POSTS";
 }
+
+let postBtn = document.getElementById("postBtn");
+
+postBtn.addEventListener("click", function () {
+  let title = document.getElementById("postTitle").value;
+  let details = document.getElementById("postDetails").value;
+  let price = document.getElementById("postPrice").value;
+
+  if (title === "" || price === "") {
+    alert("Fill title and price!");
+    return;
+  }
+
+  let post = {
+    title: title,
+    details: details,
+    price: price
+  };
+
+  if (currentTab === "need") {
+    needPosts.push(post);
+  } else {
+    offerPosts.push(post);
+  }
+});
+
